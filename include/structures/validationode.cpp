33f2b112d620de53c457cee84bcea10cb59b781b
francesco cassano <francesco.cassano2@studio.unibo.it>
2024-01-18 13:22:58 +0100

    validation node terminated, validation process to check

--- include/structures/validationode.cpp
+++ include/structures/validationode.cpp
@@ -6,6 +6,8 @@ using namespace homecare;
 const int ValidatioNode::NO_TIME = -1;
 const int ValidatioNode::FAIL = 0;
 const int ValidatioNode::OK = 1;
+const int ValidatioNode::FIRST = 0;
+const int ValidatioNode::SECOND = 1;
 
 ValidatioNode::ValidatioNode(Patient t_patient) : m_patientID (t_patient.getID()), m_sync (t_patient.getSync()),
         m_minWait (t_patient.getMinWait()), m_maxWait (t_patient.getMaxWait()) {
@@ -39,11 +41,16 @@ bool ValidatioNode::checkService(string t_service, int t_arrivalTime, int t_depa
                 if (m_services[otherI].m_completed) {
                     //TODO assicurarsi servizio 2 controllo tempi
                     if (m_sync == Simultaneous) {
-                        if (m_services[otherI].m_arrivalTime == m_services[serviceI].m_arrivalTime 
-                                && m_services[otherI].m_departureTime == m_services[serviceI].m_departureTime) {
+                        if (m_services[SECOND].m_arrivalTime == m_services[FIRST].m_arrivalTime 
+                                && m_services[SECOND].m_departureTime == m_services[FIRST].m_departureTime) {
                             return true;        
                         }
-                    } else if (m_)
+                    } else if (m_sync == Sequential) {
+                        if (m_services[SECOND].m_arrivalTime >= m_services[FIRST].m_arrivalTime + m_minWait) {
+                            return true;
+                        }
+                    }
+                    return false;
                 }         
             } 
             return true;
@@ -53,11 +60,11 @@ bool ValidatioNode::checkService(string t_service, int t_arrivalTime, int t_depa
 
 //Validate on max 2 services
 int ValidatioNode::setTime(string t_service, int t_arrivalTime, int t_departureTime) {
-    if (m_services[0].m_service == t_service) {
-        if (checkService(t_service, t_arrivalTime, t_departureTime, 0, 1)) { return OK; }
+    if (m_services[FIRST].m_service == t_service) {
+        if (checkService(t_service, t_arrivalTime, t_departureTime, FIRST, SECOND)) { return OK; }
     }
-    if (m_services.size() > 1 && m_services[1].m_service == t_service) {
-        if (checkService(t_service, t_arrivalTime, t_departureTime, 1, 0)) { return OK; }
+    if (m_services.size() > 1 && m_services[SECOND].m_service == t_service) {
+        if (checkService(t_service, t_arrivalTime, t_departureTime, SECOND, FIRST)) { return OK; }
     }
     
     return FAIL; 

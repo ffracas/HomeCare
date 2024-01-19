8c8e1f323d82d6614fa46c04cd6a9a54a499bcd4
francesco cassano <francesco.cassano2@studio.unibo.it>
2024-01-19 14:07:09 +0100

    fixed errors on checkSolution method and generateIsolution method

--- include/hc/hcvalidation.cpp
+++ include/hc/hcvalidation.cpp
@@ -67,22 +67,20 @@ bool HCValidation::checkSolution() {
         for (int i = 1; i < route_nodes.size(); ++i) {
             Node node(route_nodes[i]);
             //controlla se il servizio è disponibile per questo caregiver
-            if (find(services.begin(), services.end(), node.getService()) == services.end()) { cout<<"servizio"<<node.getService();return false; }
+            if (find(services.begin(), services.end(), node.getService()) == services.end()) { return false; }
             //controlla se il caregiver non è stato rifiutato dal paziente
             vector<string> refused = m_data.getPatient(node.getId()).getInvalidCaregivers();
-            if (find(refused.begin(), refused.end(), route.getCaregiver()) != refused.end()) { cout<<"caregiver indesiderato";return false; }
+            if (find(refused.begin(), refused.end(), route.getCaregiver()) != refused.end()) { return false; }
             //controlla che il caregiver non sia già passato per il nodo e che il tempo sia in sincrono con altri servizi
             vector<ValidatioNode>::iterator service = find_if(nodes.begin(), nodes.end(), 
                 [node] (const ValidatioNode vnode) { return vnode.getPatient() == node.getId(); } ); 
             if (service == nodes.end()) { cout<<"ripasso"; return false; }
             if (service -> setTime(node.getService(), node.getArrivalTime(), node.getDeparturTime()) 
-                    != ValidatioNode::OK) {
-                cout<<"sincronia errata"<<i<<"-"<<route.getCaregiver()<<"---";return false;        
-            }
+                    != ValidatioNode::OK) { return false; }
             if (i > 0) {
                 Node prev = route_nodes[i - 1];
                 if (prev.getDeparturTime() + m_data.getDistance(prev.getDistancesIndex(), node.getDistancesIndex()) 
-                        > node.getArrivalTime()) { cout<<"tempi consecutivi errati"; return false; }
+                        > node.getArrivalTime()) { return false; }
             }
         }
     }

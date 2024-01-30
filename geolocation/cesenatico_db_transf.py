8a3e88a220b4d232653f6b95ba886724ea13fb23
francesco cassano <francesco.cassano2@studio.unibo.it>
2024-01-30 18:44:02 +0100

    update MakeFile to make it work on Ubuntu

--- geolocation/cesenatico_db_transf.py
+++ geolocation/cesenatico_db_transf.py
@@ -1,10 +1,14 @@
 import sqlite3
 import pandas as pd
+import re
+import math
 
 conn_new_db = sqlite3.connect('unicdata.db')
 conn_old_db = sqlite3.connect('cesenatico.sqlite')
 
 df = pd.read_sql_query("SELECT * from cesenatico", conn_old_db)
+c = conn_old_db.cursor()
+print(c.execute("SELECT Indirizzo_di_domicilio, Comune_di_domicilio from cesenatico where PK_UID = 44").fetchall())
 
 c = conn_new_db.cursor()
 # Create table
@@ -15,7 +19,10 @@ c.execute('''CREATE TABLE IF NOT EXISTS addresses (patient text PRIMARY KEY, roa
 
 addresses = []
 for i in range(len(df.index)):
-    row = ("pat{}".format(i), df.loc[i, 'Indirizzo_di_domicilio'], df.loc[i, 'num'], df.loc[i, 'cap_domicilio'], df.loc[i, 'Comune_di_domicilio'], "Italia", None, None )
+    road = re.sub("\sINT[._]|\sNR[._]|\sN[._]|\s[(]\s*[)]|\s{2,}", "", df.loc[i, 'Indirizzo_di_domicilio'])
+    #road = df.loc[i, 'Indirizzo_di_domicilio']
+    cap = None if (math.isnan(df.loc[i, 'cap_domicilio'])) else int(df.loc[i, 'cap_domicilio'])
+    row = ("pat{}".format(i), road, df.loc[i, 'num'], cap, df.loc[i, 'Comune_di_domicilio'], "Italia", None, None )
     addresses.append(row)
     print(row)
 #c.executemany('INSERT INTO addresses VALUES (?, ?, ?, ?, ?, ?, ?, ?)', addresses)
